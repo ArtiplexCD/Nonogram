d527b8af5ca84e0b9f80ef9eeabf16a2f2f93d5b
ArtiplexCD <ArtJCode@gmail.com>
2024-04-30 14:37:55 +0100

    Still figuring out git but now I think i added the changes from before git version git --version git -V git --V clear
    kj
    q
    clear

--- src/Grid.java
+++ src/Grid.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Grid implements ActionListener {
-    private final Pixel[][] pixels = new Pixel[15][15];
+    //private final Pixel[][] pixels = new Pixel[15][15];
+    private final JButton[][] pixels = new JButton[15][15];
+
     private Pixel unknownPixel;
     private Pixel whitePixel;
     private Pixel blackPixel;
@@ -13,19 +15,24 @@ public class Grid implements ActionListener {
     {
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        GridLayout layout = new GridLayout(3, 4);
+        GridLayout layout = new GridLayout(15, 15);
 
         panel.setLayout(layout);
         frame.setContentPane(panel);
-        frame.setSize(525, 525);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         for (int i = 0; i < pixels.length; i++) {
             for (int j = 0; j < pixels.length; j++) {
-                pixels[i][j] = new Pixel();
+                pixels[i][j] = new JButton();
+                pixels[i][j].setBackground(Color.yellow);
+                if (i != 0 && j != 0) {
+                    panel.add(pixels[i][j]);
+                }
+                pixels[i][j].addActionListener(this);
             }
         }
 
+        frame.setSize(750, 750);
         frame.setVisible(true);
     }
     public void actionPerformed(ActionEvent e) {
--- src/Pixel.java
+++ src/Pixel.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Pixel extends JButton {
-    private int xPos;
-    private int yPos;
-    public Pixel()
-    {
-        super();
+    //private int xPos;
+    //private int yPos;
+    private Icon unknownIcon;
 
+    public Pixel(String filename)
+    {
+        super(new ImageIcon(filename));
+
+        unknownIcon = this.getIcon();
     }
 
-    public int getxPos() { return xPos; }
-    public int getyPos() { return  yPos; }
-
+    //public int getxPos() { return xPos; }
+    //public int getyPos() { return  yPos; }
 }
