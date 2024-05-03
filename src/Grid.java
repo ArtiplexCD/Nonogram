206a609d460764ca1ff0373fee6a33ed0d210c81
ArtiplexCD <ArtJCode@gmail.com>
2024-05-03 14:24:17 +0100

    Added left and right click recognition and updated actionPerformed to work with it, not sure if it oworks becuase of the macs right click being funcky

--- src/Grid.java
+++ src/Grid.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+
 
 public class Grid implements ActionListener {
     private final int gridSize = 5;
     private final Pixel[][] pixels = new Pixel[gridSize][gridSize];
+    private int lastButtonPressed = -1; // -1 is none, 1 is left and 3 is right
 
     public Grid()
     {
@@ -19,33 +20,50 @@ public class Grid implements ActionListener {
 
         for (int i = 0; i < pixels.length; i++) {
             for (int j = 0; j < pixels.length; j++) {
+
                 pixels[i][j] = new Pixel();
                 pixels[i][j].setBackground(Color.yellow);
+
                 //TODO Change if on mac to ture or false on windows because you can see what color it is on mac if true
                 pixels[i][j].setOpaque(true);
+
                 if (i != 0 && j != 0) {
                     panel.add(pixels[i][j]);
                 }
                 pixels[i][j].addActionListener(this);
-            }
+
+                pixels[i][j].addMouseListener(new MouseAdapter() {
+                    public void mousePressed(MouseEvent e) { lastButtonPressed = e.getButton(); }
+                });
         }
 
         frame.setSize(750, 750);
         frame.setVisible(true);
+        }
     }
+
     public void actionPerformed(ActionEvent e) {
         Pixel p = (Pixel) e.getSource();
         Pixel.State pState = p.getState();
         switch (pState) {
             // TODO Figure out how to differentiate between left and right click
             case unknown:
-                p.setState(Pixel.State.marked);
+                if (lastButtonPressed == 1)
+                    p.setState(Pixel.State.marked);
+                else
+                    p.setState(Pixel.State.shaded);
                 break;
             case marked:
-                p.setState(Pixel.State.unknown);
+                if (lastButtonPressed == 1)
+                    p.setState(Pixel.State.unknown);
+                else
+                    p.setState(Pixel.State.shaded);
                 break;
             case shaded:
-                p.setState(Pixel.State.unknown);
+                if (lastButtonPressed == 1)
+                    p.setState(Pixel.State.marked);
+                else
+                    p.setState(Pixel.State.unknown);
                 break;
         }
 
