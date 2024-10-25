b984c1c858e3f5f8f54ada633463d3bae310a1d3
arturj <ArtJCode@gmail.com>
2024-10-25 16:05:17 +0100

    Added bmp files back.

--- src/Driver.java
+++ src/Driver.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Driver {
 
     public static void main(String[] args) throws IOException {
-        String bmpFile = "bmpFiles/inputNameOfFileHere";
+        String bmpFile = "bmpFiles/2colour_elephant.bmp";
 
         ByteReader byteReader = new ByteReader(bmpFile);
 
